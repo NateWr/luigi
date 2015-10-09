067d465e3f6fdcba74372a867d551c4241beba5e
Nate Wright <natew@notthisway.com>
2015-10-09 18:13:41 +0100

    Split frontend functions into own load path

--- functions.php
+++ functions.php
@@ -26,72 +26,38 @@ if ( !function_exists( 'luigi_setup_theme' ) ) {
      */
     function luigi_setup_theme() {
 
-        // Include theme files
-        include_once( 'includes/template-helpers.php' );
+        luigi_load_context();
 
-        // Register the menu
         register_nav_menus(
             array(
                 'primary_menu'	=> esc_html__( 'Primary Navigation Menu', 'luigi' ),
                 'social_menu'	=> esc_html__( 'Social Profiles Menu', 'luigi' ),
             )
         );
-
-        // Enqueue the scripts and stylesheets
-        add_action( 'wp_enqueue_scripts', 'luigi_enqueue_assets' );
-
-        // Load the navigation menu walker for aria support
-        add_action( 'wp', 'luigi_load_aria_nav_walker' );
-
-        // Dequeue styles from plugins we don't need in the footer
-        add_action( 'wp_footer', 'luigi_dequeue_footer_assets' );
-
     }
     add_action( 'after_setup_theme', 'luigi_setup_theme' );
 }
 
-if ( !function_exists( 'luigi_enqueue_assets' ) ) {
+if ( !function_exists( 'luigi_load_context' ) ) {
     /**
-     * Enqueue the theme's assets
+     * Load files when required for a given context
      *
      * @since 0.0.1
      */
-    function luigi_enqueue_assets() {
-
-        // Auto-load the parent theme's style if a child theme is active
-        if ( is_child_theme() ) {
-            wp_enqueue_style( 'luigi-parent', trailingslashit( get_template_directory_uri() ) . 'style.css' );
-        }
-
-        // Enqueue active theme's CSS stylesheet
-        wp_enqueue_style( 'luigi', get_stylesheet_uri() );
-
-        // Load fonts
-        wp_enqueue_style( 'luigi-fonts', '//fonts.googleapis.com/css?family=Bilbo+Swash+Caps|Open+Sans:400,400italic,600,600italic,700,700italic&subset=latin,latin-ext');
-
-        // Enqueue frontend script
-        wp_enqueue_script( 'luigi-js', get_stylesheet_directory_uri() . '/assets/js/frontend.js', array( 'jquery' ), '0.0.1', true );
+    function luigi_load_context() {
+        add_action( 'get_header', 'luigi_load_frontend' );
     }
 }
 
-if ( !function_exists( 'luigi_load_aria_nav_walker' ) ) {
+if ( !function_exists( 'luigi_load_frontend' ) ) {
     /**
-     * Load the aria-compliant navigation menu walker class
+     * Load files required to render the frontend
      *
      * @since 0.0.1
      */
-    function luigi_load_aria_nav_walker() {
+    function luigi_load_frontend() {
+        include_once( 'includes/load-frontend.php' );
         include_once( 'lib/WAI-ARIA-Walker_Nav_Menu/aria-walker-nav-menu.php' );
-    }
-}
-
-if ( !function_exists( 'luigi_dequeue_footer_assets' ) ) {
-    /**
-     * Dequeue styles from plugins we don't need in the footer
-     *
-     * @since 0.0.1
-     */
-    function luigi_dequeue_footer_assets() {
-        wp_dequeue_style( 'bpfwp-default' );
+        include_once( 'includes/template-helpers.php' );
     }
 }
---
+++ includes/load-frontend.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Functions exclusively used on the frontend
+ *
+ * @brief These functions are only used on the frontend and so only need to be
+ *  loaded for those requests
+ *
+ * @package    luigi
+ */
+if ( !function_exists( 'luigi_enqueue_assets' ) ) {
+    /**
+     * Enqueue the theme's assets
+     *
+     * @since 0.0.1
+     */
+    function luigi_enqueue_assets() {
+
+        // Auto-load the parent theme's style if a child theme is active
+        if ( is_child_theme() ) {
+            wp_enqueue_style( 'luigi-parent', trailingslashit( get_template_directory_uri() ) . 'style.css' );
+        }
+
+        // Enqueue active theme's CSS stylesheet
+        wp_enqueue_style( 'luigi', get_stylesheet_uri() );
+
+        // Load fonts
+        wp_enqueue_style(
+			'luigi-fonts',
+			apply_filters(
+				/**
+				 * Filter the URL to load fonts. Modify this to load different
+				 * fonts, weights or subsets from Google.
+				 *
+				 * @since 0.0.1
+				 *
+				 * @param string $url The URL to the font definitions
+				 */
+				'luigi_font_uri',
+				'//fonts.googleapis.com/css?family=Bilbo+Swash+Caps|Open+Sans:400,400italic,600,600italic,700,700italic&subset=latin,latin-ext'
+			)
+		);
+
+        // Enqueue frontend script
+        wp_enqueue_script( 'luigi-js', get_stylesheet_directory_uri() . '/assets/js/frontend.js', array( 'jquery' ), '0.0.1', true );
+    }
+	add_action( 'wp_enqueue_scripts', 'luigi_enqueue_assets' );
+}
+
+if ( !function_exists( 'luigi_dequeue_footer_assets' ) ) {
+    /**
+     * Dequeue styles from plugins we don't need in the footer
+     *
+     * @since 0.0.1
+     */
+    function luigi_dequeue_footer_assets() {
+        wp_dequeue_style( 'bpfwp-default' );
+    }
+	add_action( 'wp_footer', 'luigi_dequeue_footer_assets' );
+}
