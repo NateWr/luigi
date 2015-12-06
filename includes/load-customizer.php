b4119b0f865ffea1a398bc89b4de0d6c9bd29201
Nate Wright <natew@notthisway.com>
2015-12-06 20:43:26 +0000

    Add content-layout-control support

--- includes/load-customizer.php
+++ includes/load-customizer.php
@@ -7,7 +7,19 @@
  *
  * @package    luigi
  */
-include( 'customizer/ajax-handler.php' );
+include_once( 'customizer/ajax-handler.php' );
+
+// Initialize the content-layout-control library
+include_once( get_template_directory() . '/lib/content-layout-control/content-layout-control.php' );
+CLC_Content_Layout_Control(
+	array(
+		'url' => get_template_directory_uri() . '/lib/content-layout-control',
+		'i18n' => array(
+			'close' => __( 'Close', 'luigi' ),
+			'control-toggle' => __( 'Open/close this component', 'luigi' ),
+		),
+	)
+);
 
 if ( !function_exists( 'luigi_customizer_add_controls' ) ) {
 	/**
@@ -16,7 +28,7 @@ if ( !function_exists( 'luigi_customizer_add_controls' ) ) {
 	 * @since 0.0.1
 	 */
 	function luigi_customizer_add_controls( $wp_customize ) {
-		include( 'customizer/WP_Customize_Scaled_Image_Control.php' );
+		include_once( 'customizer/WP_Customize_Scaled_Image_Control.php' );
 		$wp_customize->register_control_type( 'Luigi_WP_Customize_Scaled_Image_Control' );
 
 		$wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
@@ -58,6 +70,39 @@ if ( !function_exists( 'luigi_customizer_add_controls' ) ) {
 				)
 			)
 		);
+
+		$wp_customize->add_section(
+			'content_layout_control',
+			array(
+				'capability' => 'edit_posts',
+				'title'      => __( 'Homepage Layout', 'luigi' ),
+			)
+		);
+
+		$wp_customize->add_setting(
+			'content_layout_control',
+			array(
+				'sanitize_callback' => 'CLC_Content_Layout_Control::sanitize',
+				'transport'         => 'postMessage',
+				'type'              => 'content_layout',
+			)
+		);
+
+		$wp_customize->add_control(
+			new CLC_WP_Customize_Content_Layout_Control(
+				$wp_customize,
+				'content_layout_control',
+				array(
+					'section'    => 'content_layout_control',
+					'priority'   => 1,
+					'components' => array( 'content-block' ),
+					'i18n' => array(
+						'add_component'  => esc_html( 'Add Component', 'luigi' ),
+						'edit_component' => esc_html( 'Edit', 'luigi' ),
+					),
+				)
+			)
+		);
 	}
 	add_action( 'customize_register', 'luigi_customizer_add_controls' );
 }
@@ -75,7 +120,7 @@ if ( !function_exists( 'luigi_customizer_enqueue_preview_assets' ) ) {
 		// Maybe load minified scripts
 		$min = WP_DEBUG ? '' : 'min.';
 
-		wp_enqueue_script( 'luigi-customizer-preview-js', get_stylesheet_directory_uri() . '/assets/js/customizer-preview.' . $min . 'js', array( 'jquery', 'luigi-js' ), '0.0.1', true );
+		wp_enqueue_script( 'luigi-customizer-preview-js', get_stylesheet_directory_uri() . '/assets/js/customizer-preview.' . $min . 'js', array( 'luigi-js', 'customize-preview' ), '0.0.1', true );
 
 		$upload_dir = wp_upload_dir();
 		wp_localize_script( 'luigi-customizer-preview-js', 'luigi_theme_customizer', array(
@@ -89,13 +134,13 @@ if ( !function_exists( 'luigi_customizer_enqueue_preview_assets' ) ) {
 	add_action( 'customize_preview_init', 'luigi_customizer_enqueue_preview_assets' );
 }
 
-if ( !function_exists( 'luigi_customizer_enqueue_control_assetes' ) ) {
+if ( !function_exists( 'luigi_customizer_enqueue_control_assets' ) ) {
 	/**
 	 * Load assets to handle the customizer control panel
 	 *
 	 * @since 0.0.1
 	 */
-	function luigi_customizer_enqueue_control_assetes() {
+	function luigi_customizer_enqueue_control_assets() {
 
 		wp_enqueue_style( 'luigi-customizer-control', get_stylesheet_directory_uri() . '/assets/css/customizer-control.css', '0.0.1' );
 
@@ -104,5 +149,46 @@ if ( !function_exists( 'luigi_customizer_enqueue_control_assetes' ) ) {
 
 		wp_enqueue_script( 'luigi-customizer-control-js', get_stylesheet_directory_uri() . '/assets/js/customizer-control.' . $min . 'js', array( 'customize-controls' ), '0.0.1', true );
 	}
-	add_action( 'customize_controls_enqueue_scripts', 'luigi_customizer_enqueue_control_assetes' );
+	add_action( 'customize_controls_enqueue_scripts', 'luigi_customizer_enqueue_control_assets' );
+}
+
+if ( !function_exists( 'luigi_customizer_register_content_layout_control_components' ) ) {
+	/**
+	 * Register layout control components for the content-layout-control lib
+	 *
+	 * @since 0.0.1
+	 */
+	function luigi_customizer_register_content_layout_control_components( $components ) {
+
+		if ( !isset( $components['content-block'] ) ) {
+			$components['content-block'] = array(
+				'file'        => CLC_Content_Layout_Control::$dir . '/components/content-block.php',
+				'class'       => 'CLC_Component_Content_Block',
+				'name'        => __( 'Content Block', 'luigi' ),
+				'description' => __( 'A simple content block with an image, title, text and links.', 'luigi' ),
+				'i18n'        => array(
+					'title'                         => esc_attr__( 'Title', 'luigi' ),
+					'content'                       => esc_attr__( 'Content', 'luigi' ),
+					'image'                         => esc_attr__( 'Image', 'luigi' ),
+					'image_placeholder'             => esc_attr__( 'No image selected', 'luigi' ),
+					'image_position'                => esc_attr__( 'Image Position', 'luigi' ),
+					'image_position_left'           => esc_attr__( 'Left', 'luigi' ),
+					'image_position_right'          => esc_attr__( 'Right', 'luigi' ),
+					'image_position_background'     => esc_attr__( 'Background', 'luigi' ),
+					'image_select_button'           => esc_attr__( 'Select Image', 'luigi' ),
+					'image_change_button'           => esc_attr__( 'Change Image', 'luigi' ),
+					'image_remove_button'           => esc_attr__( 'Remove', 'luigi' ),
+					'links'                         => esc_attr__( 'Links', 'luigi' ),
+					'links_add_button'              => esc_attr__( 'Add Link', 'luigi' ),
+					'links_remove_button'           => esc_attr__( 'Remove', 'luigi' ),
+					'links_url'                     => esc_attr__( 'URL', 'luigi' ),
+					'links_text'                    => esc_attr__( 'Link Text', 'luigi' ),
+					'links_search_existing_content' => esc_attr__( 'Search existing content', 'luigi' ),
+				)
+			);
+		}
+
+		return $components;
+	}
+	add_filter( 'clc_register_components', 'luigi_customizer_register_content_layout_control_components' );
 }
--- index.php
+++ index.php
@@ -5,7 +5,10 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
-		<?php // the loop ?>
+	<?php /* Start the Loop */ ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+	<?php the_content(); ?>
+	<?php endwhile; ?>
 
 	</main><!-- #main -->
 </div><!-- #primary -->
