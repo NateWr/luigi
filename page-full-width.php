d2a432a9368d2bc9ddaf206a9c08a1f5ee929b10
Nate Wright <natew@notthisway.com>
2016-02-11 23:00:10 +0000

    Add full-width page template

---
+++ page-full-width.php
@@ -0,0 +1,29 @@
+<?php
+/**
+Template Name: Full Width
+ *
+ * The template for displaying a full-width page with no sidebar
+ *
+ * @package luigi
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<?php
+		while ( have_posts() ) : the_post();
+			get_template_part( 'content', get_post_type() );
+
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+		endwhile;
+		?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+get_footer();
