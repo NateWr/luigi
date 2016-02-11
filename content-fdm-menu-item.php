e36a9c827d830002f3168a594bb8925aaf9a4704
Nate Wright <natew@notthisway.com>
2016-02-11 23:23:17 +0000

    Add single menu item template

---
+++ content-fdm-menu-item.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying a single menu item
+ *
+ * This just calls the_content(). The Food and Drink Menu plugin will
+ * automatically return complete markup that can be used on single pages.
+ *
+ * @package luigi
+ */
+the_content();
+?>
