e67fbcc8f05910bfa21f4ecc553014e671b762d2
Nate Wright <natew@notthisway.com>
2016-03-10 19:00:36 +0000

    Fix bug with multiple maps on homepage in customizer

--- assets/src/js/content-layout-control/components/preview/luigi-map.js
+++ assets/src/js/content-layout-control/components/preview/luigi-map.js
@@ -17,11 +17,19 @@
 		* @since 0.1
 		*/
 		injectHTML: function( html ) {
+
 			html += '<a href="#" class="clc-edit-component">' + CLC_Preview_Settings.i18n.edit_component + '</a>';
-			this.$el.html( html );
+
+			// Assign a unique ID to this map (since each map is loaded in its
+			// own request, they all come back with #bp-map-0 )
+			var i = $( '.bp-map' ).length;
+			var el = $( html );
+			el.find( '#bp-map-0' ).attr( 'id', 'bp-map-' + i.toString() );
+
+			this.$el.html( el );
 
 			// Re-initialize any maps which may have been added
-			if ( typeof bp_initialize_map !== 'undefined' && typeof typeof google !== 'undefined' || typeof google.maps != 'undefined' ) {
+			if ( typeof bp_initialize_map !== 'undefined' && typeof typeof google !== 'undefined' && typeof google.maps != 'undefined' ) {
 				bp_initialize_map();
 			}
 		},
--- assets/src/js/content-layout-control/components/preview/luigi-mixer.js
+++ assets/src/js/content-layout-control/components/preview/luigi-mixer.js
@@ -30,8 +30,16 @@
 		 * @since 0.1
 		 */
 		injectHTML: function( html ) {
+
 			html += '<a href="#" class="clc-edit-component">' + CLC_Preview_Settings.i18n.edit_component + '</a>';
-			this.$el.html( html );
+
+			// Assign a unique ID to a map (since each map is loaded in its
+			// own request, they all come back with #bp-map-0 )
+			var i = $( '.bp-map' ).length;
+			var el = $( html );
+			el.find( '#bp-map-0' ).attr( 'id', 'bp-map-' + i.toString() );
+
+			this.$el.html( el );
 
 			// Re-initialize any maps which may have been added
 			if ( typeof bp_initialize_map !== 'undefined' && typeof google !== 'undefined' && typeof google.maps != 'undefined' ) {
