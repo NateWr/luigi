2d5935466b83888a97a2c45d8f0988088a55cc1c
Nate Wright <natew@notthisway.com>
2016-03-18 11:53:11 +0000

    Fix logic inversion in logo template

--- template-parts/logo.php
+++ template-parts/logo.php
@@ -12,13 +12,13 @@ $brand_element = is_front_page() ? 'h1' : 'div';
 
 <<?php echo $brand_element; ?> class="brand">
 	<a class="home-link" href="<?php echo home_url(); ?>" title="<?php esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-		<?php if ( get_theme_mod( 'site_logo' ) ) : ?>
+		<?php if ( !get_theme_mod( 'site_logo' ) ) : ?>
 			<?php echo get_bloginfo( 'name', 'display' ); ?>
 		<?php else : ?>
 			<?php luigi_print_logo(); ?>
 		<?php endif; ?>
 	</a>
-	<?php if ( get_theme_mod( 'site_logo' ) && !get_bloginfo( 'description' ) ) : ?>
+	<?php if ( !get_theme_mod( 'site_logo' ) && get_bloginfo( 'description' ) ) : ?>
 		<span class="site-tagline">
 			<?php echo get_bloginfo( 'description', 'display' ); ?>
 		</span>
