f5140567707d29ffea3998708c7bac0bc5fa1aba
Nate Wright <natew@notthisway.com>
2016-09-08 12:00:45 +0100

    Update styles for Ninja Forms 3.0

--- includes/integrations/ninja-forms.php
+++ includes/integrations/ninja-forms.php
@@ -6,8 +6,19 @@
  */
 
 /**
- * Disable default CSS files in Ninja Forms
+ * Disable default CSS files in Ninja Forms prior to version 3.0
  *
  * @since 0.1
  */
 remove_action( 'ninja_forms_display_css', 'ninja_forms_display_css');
+
+/**
+ * Disable default CSS files in Ninja Forms in versions 3.0+
+ *
+ * @since 1.1.1
+ */
+function luigi_ninja_forms_dequeue_styles() {
+    wp_dequeue_style( 'nf-display' );
+}
+
+add_action( 'nf_display_enqueue_scripts', 'luigi_ninja_forms_dequeue_styles' );
