30e3a08a23480ade8973f5326111c0ba1265d89e
Nate Wright <natew@notthisway.com>
2016-09-08 12:13:36 +0100

    Update grunt makepot task to remove extraneous options

--- Gruntfile.js
+++ Gruntfile.js
@@ -140,24 +140,16 @@ module.exports = function(grunt) {
 		makepot: {
 			target: {
 				options: {
-					cwd: '',                          // Directory of files to internationalize.
-					domainPath: 'languages',                   // Where to save the POT file.
-					exclude: [],                      // List of files or directories to ignore.
-					include: [],                      // List of files or directories to include.
-					i18nToolsPath: '/media/Storage/projects/wordpress/trunk/tools/i18n',                // Path to the i18n tools directory.
-					mainFile: 'functions.php',                     // Main project file.
-					potComments: '',                  // The copyright at the beginning of the POT file.
-					potFilename: 'luigi.pot',                  // Name of the POT file.
+					domainPath: 'languages',
 					potHeaders: {
-						poedit: true,                 // Includes common Poedit headers.
-						'x-poedit-keywordslist': true // Include a list of all possible gettext functions.
-					},                                // Headers to add to the generated POT file.
+	                    poedit: true,
+	                    'x-poedit-keywordslist': true
+	                },
 					processPot: function( pot, options ) {
-						pot.headers['report-msgid-bugs-to'] = 'http://themeofthecrop.com';
+						pot.headers['report-msgid-bugs-to'] = 'https://themeofthecrop.com';
 						return pot;
 					},
-					type: 'wp-theme',                // Type of project (wp-plugin or wp-theme).
-					updateTimestamp: true             // Whether the POT-Creation-Date should be updated without other changes.
+					type: 'wp-theme',
 				}
 			}
 		},
